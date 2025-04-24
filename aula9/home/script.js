4e8c39d13520a6e9ee54246029a5d26c4f863429
RicardoFM1 <ricardofernandesdemoura7@gmail.com>
2025-04-24 03:35:28 -0300

    atualizações

---
+++ aula9/home/script.js
@@ -0,0 +1,58 @@
+function openModal(){
+    const btn = document.querySelector(".open")
+    btn.addEventListener("click",()=>{
+        // document.body.innerHTML = `
+        //     <div class="wrapper">
+        // <div class="modal">
+        //     <h2>Olá</h2>
+        //     <p>Isso é um modal</p>
+        //     <button class="close">Fechar</button>
+        // </div>
+        // </div>
+        // `
+        document.body.insertAdjacentHTML("beforeend",`
+            
+            <div class="wrapper">
+        <div class="modal">
+            <h2>Olá</h2>
+            <p>Isso é um modal</p>
+            <button class="close">Fechar</button>
+        </div>
+        </div>
+            `)
+            const close = document.querySelector(".close2")
+           
+            close.addEventListener("click",()=>{
+               
+                const wrapper = document.querySelector(".wrapper")
+                wrapper.remove()
+            })
+    })
+    
+}
+openModal()
+
+function addCart(){
+    const btnAdd = document.querySelector(".add_cart")
+    btnAdd.addEventListener("click",()=>{
+        console.log("add")
+        document.body.insertAdjacentHTML("beforeend",`
+            
+            <div class="toast">
+                <p>Produto adicionado ao carrinho!</p>
+            </div>
+            `)
+    })
+}
+addCart()
+function getUser(){
+    const email = localStorage.getItem("email")
+    console.log(email,"email")
+    if(email){
+        const userspan = document.querySelector(".user")
+        userspan.innerText = email
+    }else{
+        location.href = "../home"
+    }
+}
+getUser()
---
+++ aula9/login/script.js
@@ -0,0 +1,31 @@
+function fazerLogin(){
+    const form = document.querySelector("form")
+    form.addEventListener("submit",(e)=>{
+        e.preventDefault()
+        console.log("submit")
+        const email = document.querySelector("#email")
+        const pass = document.querySelector("#pass")
+        console.log(email.value,pass.value)
+        if(pass.value=="1234"){
+            document.body.insertAdjacentHTML("beforeend",`
+            
+                <div class="toast success">
+                    <p>Login efetuado com sucesso!</p>
+                </div>
+                `)
+            setTimeout(()=>{
+                localStorage.setItem("email",email.value)
+                location.href = "../home"
+                
+            },3000)
+        }else {
+            document.body.insertAdjacentHTML("beforeend",`
+            
+                <div class="toast error">
+                    <p>Senha ou usuário inválidos</p>
+                </div>
+                `)
+        }
+    })
+}   
+fazerLogin()
