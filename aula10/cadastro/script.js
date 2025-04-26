cbb2d3e5ca9868ad536a2c2d67c3b87d621e3d7f
RicardoFM1 <ricardofernandesdemoura7@gmail.com>
2025-04-26 20:57:06 -0300

    siteLogin

--- aula10/cadastro/script.js
+++ aula10/cadastro/script.js
@@ -1,7 +1,9 @@
+const form = document.querySelector("#cadastro")
 function fazerCadastro(){
-    const form = document.querySelector("#cadastro")
-    form.addEventListener("submit", (evento)=>{
-        evento.preventDefault()
+    const usuarioCadastrado = JSON.parse(localStorage.getItem("usuario"))
+    const usuarioEmail = usuarioCadastrado.email
+    form.addEventListener("submit", (eventocadastro)=>{
+        eventocadastro.preventDefault()
         {
         const senha = document.querySelector("#senha")
         const email = document.querySelector("#email")
@@ -10,26 +12,24 @@ function fazerCadastro(){
        
         if(senha.value != confirmarSenha.value){
             document.body.insertAdjacentHTML("beforeend",`
-                <div id="toast">
+                <div class="toast erro">
                     <p>Confira se a senhas estão idênticas</p>
                 </div>
                 `)
         }else if(senha.value ===  "" || email.value === ""){
             document.body.insertAdjacentHTML("beforeend",`
-                <div id="toast">
+                <div class="toast erro">
                     <p>Insira uma senha ou email válido</p>
                 </div>
                 `)
-        
-        
         }else if(tamanhoSenha.length < 8){
             document.body.insertAdjacentHTML("beforeend", `
-                <div id="toast">
-                    <p>A senha precisa ter no mínimo 8 caracteres!</p>
+                <div class="toast erro">
+                     <p>A senha precisa ter no mínimo 8 caracteres!</p>
                 </div>
-                `)
-        }
-        else{
+               `)
+        
+        }else{
             const user  = {
                 senha: senha.value,
                 email : email.value
@@ -39,12 +39,21 @@ function fazerCadastro(){
                 location.href = "/login"
             },3000)
             document.body.insertAdjacentHTML("beforeend", `
-                <div id="toast">
+                <div class="toast sucesso">
                     <p>Usuário cadastrado com sucesso!</p>
                 </div>
                 `)
-            
+               
     }}})
+//     if(email === usuarioEmail){
+//         document.body.insertAdjacentHTML("beforeend", `
+//             <div class="toast erro">
+//                 <p>Email já cadastrado!</p>   -- ARRUMAR ERRO DE CONSEGUIR LOGAR MESMO COM UM EMAIL JÁ CADASTRADO.
+//             </div>
+//             `)
+            
+// }
+    console.log(usuarioCadastrado, usuarioEmail)
 }
 
 fazerCadastro()
--- aula10/login/script.js
+++ aula10/login/script.js
@@ -1,17 +1,31 @@
-const usuarios = localStorage.getItem("usuario")
-const senhaLogin = document.querySelector(".senha")
-const emailLogin = document.querySelector(".email")
-const usuariosemobj = JSON.parse(usuarios)
-    if(senhaLogin.value || emailLogin.value != usuarios){
-    document.body.insertAdjacentHTML("beforeend", `
-        <div id="toast"
-            <p>Esse usuário não existe</p>
-        </div>
-        `)
-    }else{
+function Login(){
+    const usuario = JSON.parse(localStorage.getItem("usuario")) //== object: email: ricardofernandes10@gmail.com senha: 11111111111
+    const usuarioSenha = usuario.senha 
+    const usuarioEmail = usuario.email    
+    const form = document.querySelector(".login")
+    form.addEventListener("submit", (eventologin)=>{
+        eventologin.preventDefault()
+        {
+    const senhaLogin = document.querySelector(".senha")
+    const emailLogin = document.querySelector(".email")
+    if(senhaLogin.value == usuarioSenha && emailLogin.value == usuarioEmail){
         document.body.insertAdjacentHTML("beforeend", `
-            <div id="toast"
-                <p>Login efetuado com sucesso</p>
+            <div class="toast sucesso">
+                <p>Login efetuado com sucesso!</p>
+                <p>Redirecionando para a página principal.</p
             </div>
             `)
+        setTimeout(()=>{
+            location.href = "/"
+        },3000)
+        }else{
+            document.body.insertAdjacentHTML("beforeend", `
+                <div class="toast erro">
+                    <p>Este usuário não existe!</p>
+                </div>
+                `)
+            }
+        }})
+        console.log(usuarioEmail, usuarioSenha, usuario)
     }
+Login()
