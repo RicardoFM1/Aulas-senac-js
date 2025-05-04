0c2dddcf6e525f066bfe3cf07659e6ea6d7f8f65
RicardoFM1 <ricardofernandesdemoura7@gmail.com>
2025-05-04 05:30:29 -0300

    Pokedéx

--- aula11/script.js
+++ aula11/script.js
@@ -1,5 +1,5 @@
 async function pegarPokemons() {
-    const pokemons = await fetch("https://pokeapi.co/api/v2/pokemon/", {
+    const pokemons = await fetch("https://pokeapi.co/api/v2/pokemon/?offset=20&limit=20", {
         headers: {
             'Accept': "application/json"
         }
@@ -10,7 +10,7 @@ async function pegarPokemons() {
     const ul = document.querySelector("ul");
 
     for (const item of listaPokemon) {
-        const data = await fetch(item.url);
+        const data = await fetch(item.url);     
         const dados = await data.json();
 
         ul.insertAdjacentHTML("beforeend", `
@@ -31,15 +31,33 @@ async function pegarPokemons() {
             } else {
                 
                 const habilidades = await fetch(dados.abilities[0].ability.url)
+                const habilidades2 = await fetch(dados.abilities[1].ability.url)
+                const Locais = await fetch(dados.location_area_encounters)
                 console.log(habilidades)
+                console.log(habilidades2)
+                console.log(Locais)
                 const habilidadesJson = await habilidades.json()
+                const habilidades2Json = await habilidades2.json()
+                const localJson = await Locais.json()
                 console.log(habilidadesJson.effect_entries[0].effect)
+                console.log(habilidades2Json.effect_entries[0].effect)
+                
 
                 document.body.insertAdjacentHTML("beforeend", `
                     <div id="modal"> 
-                        <p class="nomePokeInfo">${item.name}</p>
-                        <p class="habilidadePoke"><strong><em>Habilidade:</em></strong> ${dados.abilities[0].ability.name}</p>
-                        <p class="habilidadeEfeito"><strong><em>Efeito:</em></strong> ${habilidadesJson.effect_entries[0].effect}</p>
+                    <div class="informacoesPoke">
+                        <h1>${item.name}</h1>
+                        <p><strong><em>Habilidade:</em></strong> ${dados.abilities[0].ability.name}</p>
+                        <p><strong><em>Efeito:</em></strong> ${habilidadesJson.effect_entries[0].effect}</p>
+                        <p><strong><em>Efeito rápido:</em></strong> ${habilidadesJson.effect_entries[0].short_effect}</p>
+                        <p><strong><em>Habilidade:</em></strong> ${dados.abilities[1].ability.name}</p>
+                        <p><strong><em>Efeito:</em></strong> ${habilidades2Json.effect_entries[0].effect}</p>
+                        <p><strong><em>Efeito rápido:</em></strong> ${habilidades2Json.effect_entries[0].short_effect}</p>
+                        <p><strong><em>Experiência base:</em></strong> ${dados.base_experience}</p>
+                        <p><strong><em>índice do jogo:</em></strong> ${dados.game_indices[0].game_index}</p>
+                        <p><strong><em>Onde encontrar:</em></strong> ${localJson[0].location_area.name}, <strong><em>chance de encontrar:</em></strong> ${localJson[0].version_details[0].encounter_details[0].chance}%,
+                        <strong><em>Nível máximo:</em></strong> ${localJson[0].version_details[0].encounter_details[0].max_level},  <strong><em>método:</em></strong> ${localJson[0].version_details[0].encounter_details[0].method.name}, 
+                        <strong><em>Nível mínimo:</em></strong> ${localJson[0].version_details[0].encounter_details[0].min_level}</p>
                         <p>teste</p>
                         <p>teste</p>
                         <p>teste</p>
@@ -67,16 +85,7 @@ async function pegarPokemons() {
                         <p>teste</p>
                         <p>teste</p>
                         <p>teste</p>
-                        <p>teste</p>
-                        <p>teste</p>
-                        <p>teste</p>
-                        <p>teste</p>
-                        <p>teste</p>
-                        <p>teste</p>
-                        <p>teste</p>
-                        
-
-
+                    </div>
                         <div id="button_info">
                             <button class="buttonfechar">Fechar</button>
                         </div>
