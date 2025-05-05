bc5411c30fea8a4c527cebd79dc31020fbb1ec97
= <=>
2025-05-05 20:15:34 -0300

    Pokedéx

--- aula11/script.js
+++ aula11/script.js
@@ -1,102 +1,251 @@
 async function pegarPokemons() {
-    const pokemons = await fetch("https://pokeapi.co/api/v2/pokemon/?offset=20&limit=20", {
-        headers: {
-            'Accept': "application/json"
-        }
-    });
+  const pokemons = await fetch(
+    "https://pokeapi.co/api/v2/pokemon/?offset=20&limit=20",
+    {
+      headers: {
+        Accept: "application/json",
+      },
+    }
+  );
 
-    const pokemonRes = await pokemons.json();
-    const listaPokemon = pokemonRes.results;
-    const ul = document.querySelector("ul");
+  const pokemonRes = await pokemons.json();
+  const listaPokemon = pokemonRes.results;
+  const ul = document.querySelector("ul");
 
-    for (const item of listaPokemon) {
-        const data = await fetch(item.url);     
-        const dados = await data.json();
+  for (const item of listaPokemon) {
+    const data = await fetch(item.url);
+    const dados = await data.json();
 
-        ul.insertAdjacentHTML("beforeend", `
+    ul.insertAdjacentHTML(
+      "beforeend",
+      `
             <li id="${item.name}">
                 <p class="nomePoke">${item.name}</p>
                 <img src="${dados.sprites.front_shiny}">
                 <button class="info">Mais informações</button> 
             </li>
-        `);
+        `
+    );
 
-        const buttonInfo = document.getElementById(item.name);
-        const button = buttonInfo.querySelector(".info");
+    const buttonInfo = document.getElementById(item.name);
+    const button = buttonInfo.querySelector(".info");
 
-        button.addEventListener("click", async () => {
-             
-            if (document.querySelector("#modal")) {
-                modal.remove();
-            } else {
-                
-                const habilidades = await fetch(dados.abilities[0].ability.url)
-                const habilidades2 = await fetch(dados.abilities[1].ability.url)
-                const Locais = await fetch(dados.location_area_encounters)
-                const Movimento1 = await fetch(dados.moves[0].move.url)
-                const Movimento2 = await fetch(dados.moves[1].move.url)
-                const Movimento3 = await fetch(dados.moves[2].move.url)
-                const Movimento4 = await fetch(dados.moves[3].move.url)
-                const Movimento5 = await fetch(dados.moves[4].move.url)
-                console.log(habilidades)
-                console.log(habilidades2)
-                console.log(Locais)
-                console.log(Movimento1)
-                const habilidadesJson = await habilidades.json()
-                const habilidades2Json = await habilidades2.json()
-                const localJson = await Locais.json()
-                const movimentosJson1 = await Movimento1.json()
-                const movimentosJson2 = await Movimento2.json()
-                const movimentosJson3 = await Movimento3.json()
-                const movimentosJson4 = await Movimento4.json()
-                const movimentosJson5 = await Movimento5.json()
-                console.log(habilidadesJson.effect_entries[0].effect)
-                console.log(habilidades2Json.effect_entries[0].effect)
-                console.log(movimentosJson1)
-                
+    button.addEventListener("click", async () => {
+      if (document.querySelector("#modal")) {
+        modal.remove();
+      } else {
+        const habilidades = await fetch(dados.abilities[0].ability.url);
+        const habilidades2 = await fetch(dados.abilities[1].ability.url);
+        const Locais = await fetch(dados.location_area_encounters);
+        const Movimento1 = await fetch(dados.moves[0].move.url);
+        const Movimento2 = await fetch(dados.moves[1].move.url);
+        const Movimento3 = await fetch(dados.moves[2].move.url);
+        const Movimento4 = await fetch(dados.moves[3].move.url);
+        const Movimento5 = await fetch(dados.moves[4].move.url);
+        console.log(habilidades);
+        console.log(habilidades2);
+        console.log(Locais);
+        console.log(Movimento1);
+        const habilidadesJson = await habilidades.json();
+        const habilidades2Json = await habilidades2.json();
+        const localJson = await Locais.json();
+        const movimentosJson1 = await Movimento1.json();
+        const movimentosJson2 = await Movimento2.json();
+        const movimentosJson3 = await Movimento3.json();
+        const movimentosJson4 = await Movimento4.json();
+        const movimentosJson5 = await Movimento5.json();
+        console.log(habilidadesJson.effect_entries[0].effect);
+        console.log(habilidades2Json.effect_entries[0].effect);
+        console.log(movimentosJson1);
 
-                document.body.insertAdjacentHTML("beforeend", `
+        document.body.insertAdjacentHTML(
+          "beforeend",
+          `
                     <div id="modal"> 
                     <div class="informacoesPoke">
                         <h1>${item.name}</h1>
-                        <p><strong><em>Habilidade:</em></strong> ${dados.abilities[0].ability.name}</p>
-                        <p><strong><em>Efeito:</em></strong> ${habilidadesJson.effect_entries[0].effect}</p>
-                        <p><strong><em>Efeito rápido:</em></strong> ${habilidadesJson.effect_entries[0].short_effect}</p>
-                        <p><strong><em>Habilidade:</em></strong> ${dados.abilities[1].ability.name}</p>
-                        <p><strong><em>Efeito:</em></strong> ${habilidades2Json.effect_entries[0].effect}</p>
-                        <p><strong><em>Efeito rápido:</em></strong> ${habilidades2Json.effect_entries[0].short_effect}</p>
-                        <p><strong><em>Experiência base:</em></strong> ${dados.base_experience}</p>
-                        <p><strong><em>índice do jogo:</em></strong> ${dados.game_indices[0].game_index}</p>
-                        <p><strong><em>Onde encontrar:</em></strong> ${localJson[0].location_area.name}, <strong><em>chance de encontrar:</em></strong> ${localJson[0].version_details[0].encounter_details[0].chance}%,
-                        <strong><em>Nível máximo:</em></strong> ${localJson[0].version_details[0].encounter_details[0].max_level},  <strong><em>método:</em></strong> ${localJson[0].version_details[0].encounter_details[0].method.name}, 
-                        <strong><em>Nível mínimo:</em></strong> ${localJson[0].version_details[0].encounter_details[0].min_level}</p>
-                        <p><strong><em>Movimentos:</em></strong> ${dados.moves[0].move.name}, ${dados.moves[1].move.name}, ${dados.moves[2].move.name}, ${dados.moves[3].move.name}, ${dados.moves[4].move.name} </p>
-                        <p><strong><em>Movimento 1:</em></strong> ${movimentosJson1.accuracy}<strong><em>% de precisão,</em></strong> <strong><em>efeito do movimento:</em></strong> ${movimentosJson1.effect_entries[0].effect}</p>
-                        <p><strong><em>Movimento 2:</em></strong> ${movimentosJson2.accuracy}<strong><em>% de precisão,</em></strong> <strong><em>efeito do movimento:</em></strong> ${movimentosJson2.effect_entries[0].effect}</p>
-                        <p><strong><em>Movimento 3:</em></strong> ${movimentosJson3.accuracy}<strong><em>% de precisão,</em></strong> <strong><em>efeito do movimento:</em></strong> ${movimentosJson3.effect_entries[0].effect}</p>
-                        <p><strong><em>Movimento 4:</em></strong> ${movimentosJson4.accuracy}<strong><em>% de precisão,</em></strong> <strong><em>efeito do movimento:</em></strong> ${movimentosJson4.effect_entries[0].effect}</p>
-                        <p><strong><em>Movimento 5:</em></strong> ${movimentosJson5.accuracy}<strong><em>% de precisão,</em></strong> <strong><em>efeito do movimento:</em></strong> ${movimentosJson5.effect_entries[0].effect}</p>
-                        <p><strong><em>Método de aprendizagem do movimento 1:</em></strong> ${dados.moves[0].version_group_details[0].move_learn_method.name}</p>
-                        <p><strong><em>Método de aprendizagem do movimento 2:</em></strong> ${dados.moves[0].version_group_details[1].move_learn_method.name}</p>
-                        <p><strong><em>Método de aprendizagem do movimento 3:</em></strong> ${dados.moves[0].version_group_details[2].move_learn_method.name},</p>
-                        <p><strong><em>Método de aprendizagem do movimento 4:</em></strong> ${dados.moves[0].version_group_details[3].move_learn_method.name},</p>
-                        <p><strong><em>Método de aprendizagem do movimento 5:</em></strong> ${dados.moves[0].version_group_details[4].move_learn_method.name},</p>
+                        <p>
+                        <strong><em>Habilidade:</em></strong> ${
+                          dados.abilities[0].ability.name
+                        }
+                        </p>
+
+                        <p>
+                        <strong><em>Efeito:</em></strong> 
+                        ${habilidadesJson.effect_entries[0].effect}
+                        </p>
+
+                        <p>
+                        <strong><em>Efeito rápido:</em></strong> 
+                        ${habilidadesJson.effect_entries[0].short_effect}
+                        </p>
+
+                        <p>
+                        <strong><em>Habilidade:</em></strong> 
+                        ${
+                          dados.abilities[1]
+                            ? dados.abilities[1].ability.name
+                            : "Não há"
+                        }
+                        </p>
+
+                        <p>
+                        <strong><em>Efeito:</em></strong> 
+                        ${
+                          habilidades2Json.effect_entries[0]
+                            ? habilidades2Json.effect_entries[0].effect
+                            : "Não há"
+                        }
+                        </p>
+
+                        <p>
+                            <strong><em>Efeito rápido:</em></strong> 
+                            ${
+                              habilidades2Json.effect_entries[0]
+                                ? habilidades2Json.effect_entries[0]
+                                    .short_effect
+                                : "Não há"
+                            }
+                        </p>
+
+                        <p>
+                            <strong><em>Experiência base:</em></strong> 
+                            ${
+                              dados.base_experience
+                                ? dados.base_experience
+                                : "Não há"
+                            }
+                        </p>
+
+                        <p>
+                            <strong><em>índice do jogo:</em></strong> ${
+                              dados.game_indices[0]
+                                ? dados.game_indices[0].game_index
+                                : "Não há"
+                            }
+                        </p>
+
+                        <p>
+                        <strong><em>Onde encontrar:</em></strong> ${
+                          localJson[0]?.location_area
+                            ? localJson[0]?.location_area.name
+                            : "Não há"
+                        }, <strong><em>chance de encontrar:</em></strong> 
+                        ${
+                          localJson[0]?.version_details[0]
+                            ? localJson[0]?.version_details[0]
+                                .encounter_details[0].chance
+                            : "Não há"
+                        }%,
+                        <strong><em>Nível máximo:</em></strong> 
+                        ${
+                          localJson[0]?.version_details[0]
+                            ? localJson[0]?.version_details[0]
+                                .encounter_details[0].max_level
+                            : "Não há"
+                        },  
+                        <strong><em>método:</em></strong> ${
+                          localJson[0]?.version_details[0]
+                            ? localJson[0]?.version_details[0]
+                                .encounter_details[0].method.name
+                            : "Não há"
+                        }, 
+                        <strong><em>Nível mínimo:</em></strong> ${
+                          localJson[0]?.version_details[0]
+                            ? localJson[0]?.version_details[0]
+                                .encounter_details[0].min_level
+                            : "Não há"
+                        }
+                        </p>
+
+                        <p><strong><em>Movimentos:</em></strong> ${
+                          dados.moves[0] ? dados.moves[0].move.name : "Não há"
+                        }, ${
+            dados.moves[1] ? dados.moves[1].move.name : "Não há"
+          }, ${dados.moves[2] ? dados.moves[2].move.name : "Não há"}, ${
+            dados.moves[3] ? dados.moves[3].move.name : "Não há"
+          }, ${dados.moves[4] ? dados.moves[4].move.name : "Não há"} </p>
+                        <p><strong><em>Movimento 1:</em></strong> ${
+                          movimentosJson1.accuracy
+                        }<strong><em>% de precisão,</em></strong> <strong><em>efeito do movimento:</em></strong> ${
+            movimentosJson1.effect_entries[0]
+              ? movimentosJson1.effect_entries[0].effect
+              : "Não há"
+          }</p>
+                        <p><strong><em>Movimento 2:</em></strong> ${
+                          movimentosJson2.accuracy
+                        }<strong><em>% de precisão,</em></strong> <strong><em>efeito do movimento:</em></strong> ${
+            movimentosJson2.effect_entries[0]
+              ? movimentosJson2.effect_entries[0].effect
+              : "Não há"
+          }</p>
+                        <p><strong><em>Movimento 3:</em></strong> ${
+                          movimentosJson3.accuracy
+                        }<strong><em>% de precisão,</em></strong> <strong><em>efeito do movimento:</em></strong> ${
+            movimentosJson3.effect_entries[0]
+              ? movimentosJson3.effect_entries[0].effect
+              : "Não há"
+          }</p>
+                        <p><strong><em>Movimento 4:</em></strong> ${
+                          movimentosJson4.accuracy
+                        }<strong><em>% de precisão,</em></strong> <strong><em>efeito do movimento:</em></strong> ${
+            movimentosJson4.effect_entries[0]
+              ? movimentosJson4.effect_entries[0].effect
+              : "Não há"
+          }</p>
+                        <p><strong><em>Movimento 5:</em></strong> ${
+                          movimentosJson5.accuracy
+                        }<strong><em>% de precisão,</em></strong> <strong><em>efeito do movimento:</em></strong> ${
+            movimentosJson5.effect_entries[0]
+              ? movimentosJson5.effect_entries[0].effect
+              : "Não há"
+          }</p>
+                        <p><strong><em>Método de aprendizagem do movimento 1:</em></strong> ${
+                          dados.moves[0]
+                            ? dados.moves[0].version_group_details[0]
+                                ?.move_learn_method.name
+                            : "Não há"
+                        }</p>
+                        <p><strong><em>Método de aprendizagem do movimento 2:</em></strong> ${
+                          dados.moves[0]
+                            ? dados.moves[0].version_group_details[1]
+                                ?.move_learn_method.name
+                            : "Não há"
+                        }</p>
+                        <p><strong><em>Método de aprendizagem do movimento 3:</em></strong> ${
+                          dados.moves[0]
+                            ? dados.moves[0].version_group_details[2]
+                                ?.move_learn_method.name
+                            : "Não há"
+                        },</p>
+                        <p><strong><em>Método de aprendizagem do movimento 4:</em></strong> ${
+                          dados.moves[0]
+                            ? dados.moves[0].version_group_details[3]
+                                ?.move_learn_method.name
+                            : "Não há"
+                        },</p>
+                        <p><strong><em>Método de aprendizagem do movimento 5:</em></strong> ${
+                          dados.moves[0]
+                            ? dados.moves[0].version_group_details[4]
+                                ?.move_learn_method.name
+                            : "Não há"
+                        },</p>
                     </div>
                         <div id="button_info">
                             <button class="buttonfechar">Fechar</button>
                         </div>
                     </div>
-                `);
+                `
+        );
 
-                
-                const buttonFechar = document.querySelector(".buttonfechar");
-                buttonFechar.addEventListener("click", () => {
-                    const modal = document.querySelector("#modal");
-                    if (modal) modal.remove();
-                });
-            }
+        const buttonFechar = document.querySelector(".buttonfechar");
+        buttonFechar.addEventListener("click", () => {
+          const modal = document.querySelector("#modal");
+          if (modal) modal.remove();
         });
-    }
+      }
+    });
+  }
 }
 
 pegarPokemons();
